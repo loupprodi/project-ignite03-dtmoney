ec463f8acead117648f96d11a25b43dbd8849390
Luiz Prosdoskimi <luizprosdoskimi@gmail.com>
2023-06-29 20:10:21 -0300

    add lib phosphor e criando o comp summary e a pagina principal

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,31 @@
 import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
+import { TransactionsContainer, TransactionsTable } from "./styles";
 
 export const Transactions = () => {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>R$ 12.000,00</td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Hambirger</td>
+              <td>- R$ 59,00</td>
+              <td>Alimentação</td>
+              <td>13/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </div>
+  );
 };
---
+++ src/pages/Transactions/styles.ts
@@ -0,0 +1,14 @@
+import { styled } from "styled-components";
+
+export const TransactionsContainer = styled.main`
+  width: 100%;
+  max-width: 1120px;
+  margin: 4rem auto 0;
+  padding: 0 1.5rem;
+`;
+
+export const TransactionsTable = styled.table`
+  width: 100%;
+  border-collapse: separate;
+  border-spacing: 0 0.5rem;
+`;
