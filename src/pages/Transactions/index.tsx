39511fec56034c170dace7f7c4942e1c5a00d8c9
Luiz Prosdoskimi <luizprosdoskimi@gmail.com>
2023-07-03 23:09:21 -0300

    criando mais um registro no node-server e o utils formatter

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -8,6 +8,7 @@ import {
   TransactionsTable,
 } from "./styles";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { priceFormatter, dateFormatter } from "../../utils/formatter";
 
 export const Transactions = () => {
   const { transactions } = useContext(TransactionsContext);
@@ -27,11 +28,12 @@ export const Transactions = () => {
                   <td width="50%">{transaction.description}</td>
                   <td>
                     <PriceHighLight variant={transaction.type}>
-                      {transaction.price}
+                      {transaction.type === "outcome" && "- "}
+                      {priceFormatter.format(transaction.price)}
                     </PriceHighLight>
                   </td>
                   <td>{transaction.category}</td>
-                  <td>{transaction.createdAt}</td>
+                  <td>{dateFormatter.format(transaction.createdAt)}</td>
                 </tr>
               );
             })}
